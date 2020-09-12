ba7c72e0209e28bec7745effab2bec975f9e5054
khang <mngu3698@uni.sydney.edu.au>
2020-09-12 12:03:20 +1000

    new

---
+++ lec6/simpson_paradox_ini.R
@@ -0,0 +1,12 @@
+# Coding example
+
+# Simpson's paradox
+
+library(tidyverse)
+# install.packages("palmerpenguins")
+library(palmerpenguins)
+?penguins
+# https://github.com/allisonhorst/palmerpenguins
+glimpse(penguins)
+
+# plot bill length against depth
